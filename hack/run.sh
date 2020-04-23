6b1d5f84c46c07d6756310a7c792d47fbc2cb4fb
narvae <wexlernarvae@gmail.com>
2020-04-22 22:58:31 -0700

    nits

--- hack/run.sh
+++ hack/run.sh
@@ -6,27 +6,27 @@ pushd .
 # this should be run at the project dir
 # cd ..
 
-# up AGW VM
+# up AGW VM; tab 1
 pushd .
 cd lte/gateway
 vagrant up magma
 popd
 
-# build orc8r containers
+# build orc8r containers; tab 2
 pushd .
 cd orc8r/cloud/docker
 python ./build.py -a  # default py 3.7.4
 popd
 
 ## initial run ##
-# build AGW from source
+# build AGW from source; tab 1
 pushd .
 vagrant ssh magma
 cd magma/lte/gateway # MAGMA-VM
 make run             # MAGMA-VM
 popd
 
-# start orc8r
+# start orc8r; tab 2
 docker-compose up -d
 ls ../../../.cache/test_certs
 open ../../../.cache/test_certs
